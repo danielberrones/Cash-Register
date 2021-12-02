f8a642cda1b6af72c4aaf3e21da298a25333ebfd
DBerrones <53327546+danielberrones@users.noreply.github.com>
2021-12-02 16:16:49 -0600

    refactored, almost solved

--- main.js
+++ main.js
@@ -1,27 +1,69 @@
 function checkCashRegister(price, cash, cid) {
-  for (let i = 0; i < cid.length; i++) {
-    // console.log(cid[i]); // ["PENNY", 1.01], ["NICKEL", 2.05] ["DIME", 3.1], 
-    // console.log(cid[i][0]); // "PENNY", "NICKEL", "DIME", etc
-    console.log(cid[i][1]); // 1.01, 2.05, 3.1     $ amount in 2d array, 1st index
-    // console.log('end');
-  }
-  // console.log(cid.length);
-    // return `${price}\n${cash}\n${cid[2][1]}`;
-    return '';
-  }
-  
-let c = checkCashRegister(19.5, 20, [
-  ["PENNY", 1.01], 
-  ["NICKEL", 2.05], 
-  ["DIME", 3.1], 
-  ["QUARTER", 4.25], 
-  ["ONE", 90], 
-  ["FIVE", 55], 
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
-  ]
-);
+    let stat = ["INSUFFICIENT_FUNDS","CLOSED","OPEN"];
+    let changeArr = [];
+    let paymentDiff = cash - price;
+    // sum of cash-in-drawer 
+    let cashInDrawerSum = 0;
+    for (let i in cid) {
+        cashInDrawerSum += cid[i][1];
+    }
+    if (cashInDrawerSum < paymentDiff || paymentDiff % cashInDrawerSum !== 0) return {status:stat[0], change:changeArr};
+    if (cashInDrawerSum == paymentDiff) return {status:stat[1], change:cid};
+    // console.log(cashInDrawerSum,'cashInDrawerSum');
+    // console.log(paymentDiff,'paymentDiff');
+}
 
-console.log(c);
 
+
+
+
+
+// RETURN OBJECT
+// checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) 
+// should return an object.
+// 335.40999999999997 cashInDrawerSum
+// 0.5 paymentDiff
+
+
+
+
+
+// OPEN
+// checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) 
+// should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+// 335.40999999999997 cashInDrawerSum
+// 0.5 paymentDiff
+
+// checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) 
+// should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+// 335.40999999999997 cashInDrawerSum
+// 96.74 paymentDiff
+
+
+
+
+
+
+
+//INSUFFICIENT_FUNDS
+// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
+// should return {status: "INSUFFICIENT_FUNDS", change: []}.
+// 0.01 cashInDrawerSum
+// 0.5 paymentDiff
+
+// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
+// should return {status: "INSUFFICIENT_FUNDS", change: []}.
+// 1.01 cashInDrawerSum
+// 0.5 paymentDiff
+
+
+
+
+
+
+
+//CLOSED
+// checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
+// should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
+// 0.5 cashInDrawerSum
+// 0.5 paymentDif
