1d4a247fa23aa2cf1045a68712d8bb0eb171cf44
DBerrones <53327546+danielberrones@users.noreply.github.com>
2021-12-08 00:51:06 -0600

    Update main1.js

--- main1.js
+++ main1.js
@@ -1,5 +1,5 @@
 function cashReg(cost,price,cid) {
-    stat = {insufficientFunds:"INSUFFICIENT_FUNDS",closed:"CLOSED",open:"OPEN"};
+    let stat = {insufficientFunds:"INSUFFICIENT_FUNDS",closed:"CLOSED",open:"OPEN"};
     let priceDiff = price - cost;
     let customerHasEnough = price > cost;
     let sum = 0;
@@ -8,14 +8,14 @@ function cashReg(cost,price,cid) {
     }
     let adjSum = parseFloat(sum).toFixed(2);
     let enoughCash = adjSum > priceDiff;
-    
     console.log('◆◆◆◆  CASH REGISTER  ◆◆◆◆')
     console.log(`cost: ${cost}`);
     console.log(`amount paid: ${price}`);
     console.log(`customer's change: ${priceDiff}`);
     console.log(`money in cash register: ${adjSum}`);
     console.log(cid.reverse());
-    
+
+
     for (let i of cid) {
         if (i[0] == 'ONE HUNDRED') {
             let count = 0;
@@ -124,29 +124,5 @@ function cashReg(cost,price,cid) {
     console.log(`does cash register have enough money to return customer's change? ${enoughCash}`);
 }
 
-cashReg(103, 100, [["PENNY", .22], ["NICKEL", 12.85], ["DIME", 23.1], ["QUARTER", 4.75], ["ONE", 250], ["FIVE", 65], ["TEN", 210], ["TWENTY", 120], ["ONE HUNDRED", 300]]);
 
-// ◆◆◆◆  CASH REGISTER  ◆◆◆◆
-// cost: 103
-// amount paid: 100
-// customer's change: -3
-// money in cash register: 985.92
-// [ [ 'ONE HUNDRED', 300 ],
-//   [ 'TWENTY', 120 ],
-//   [ 'TEN', 210 ],
-//   [ 'FIVE', 65 ],
-//   [ 'ONE', 250 ],
-//   [ 'QUARTER', 4.75 ],
-//   [ 'DIME', 23.1 ],
-//   [ 'NICKEL', 12.85 ],
-//   [ 'PENNY', 0.22 ] ]
-// 100 dollar bills in cash register: 3
-// 20 dollar bills in cash register: 6
-// 10 dollar bills in cash register: 21
-// 5 dollar bills in cash register: 13
-// 1 dollar bills in cash register: 250
-// dimes in cash register: 231
-// nickels in cash register: 257
-// pennies in cash register: 22
-// does customer have enough money to pay? false
-// does cash register have enough money to return customer's change? true
+cashReg(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
