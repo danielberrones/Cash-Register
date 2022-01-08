b28ff2735a9d573f9deb4d776f30c719680783f1
db <daniel.a.berrones@gmail.com>
2022-01-07 19:06:03 -0600

    refactored

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 function checkCashRegister(price, cash, cid) {
+    // cash values
+    let cashValues = {  "ONE HUNDRED":100,
+                        "TWENTY":20,
+                        "TEN":10,
+                        "FIVE":5,
+                        "ONE":1,
+                        "QUARTER":.25,
+                        "DIME":.10,
+                        "NICKEL":.05,
+                        "PENNY":.01
+                    }
+
     // high to low
     cid.reverse()
 
@@ -11,8 +23,22 @@ function checkCashRegister(price, cash, cid) {
     for (let i of cid) {
         cidSum += i[1];
     }
-    cidSum = parseInt(cidSum).toFixed(2);
-    
+    cidSum = parseFloat(cidSum).toFixed(2);
+
+    // main algo
+    // while (change >= 0) {
+    //     for (let n of cid) {
+    //         // console.log(n[1]);
+    //     } 
+    // }
+    for (let j = 0; j < change; j+=cashValues["TWENTY"]) {
+        for (let n of cid) {
+            if (n[1] == "TWENTY") {
+                console.log(n[1]);
+            }
+        }
+        // console.log(j);
+    } 
    
     console.log(`price ${price} cash ${cash}`);
     console.log(`change ${change} cidSum ${cidSum}`);
@@ -21,7 +47,7 @@ function checkCashRegister(price, cash, cid) {
   
 //   let result = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
  
-  let result1 = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+  let result1 = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 120], ["ONE HUNDRED", 100]])
 
   
 
