8e42cfbf441090f269f9aa1204dfe37b4f9121ae
danielberrones <daniel.a.berrones@gmail.com>
2022-01-25 23:14:40 -0600

    refactored

--- f/main.js
+++ f/main.js
@@ -1,11 +1,13 @@
-function checkCashRegister(price, cash, cid) {
-	var countArr = [];
+function checkCashRegister(price, cash, cid) { var countArr = [];
 	cid.reverse();
 	var output = {status: '', change: []};
+	let change = cash - price;
+	console.log('cash',cash);
+	console.log('price',price);
+	console.log('change',change);
 	for (let i of cid) {
 		console.log(i);
 	}
-	let change = cash - price;
 	let sum = 0;
 	for (let i of cid) {
 		sum += i[1];
@@ -13,23 +15,14 @@ function checkCashRegister(price, cash, cid) {
 //	console.log(sum);
 	sum = parseFloat(sum).toFixed(2);
 
-	if (sum < change || change % sum !== 0) {
-		output.status = 'INSUFFICIENT_FUNDS';
-		return output;
-	}
-	else if (change === sum) {
-		output.status = 'CLOSED';
-		output.change = cid;
-	} 
-	else {
-		for (let i of cid) {
-    if (i[0] == 'ONE HUNDRED') {
-      let count = 0;
-      let max = i[1];
-      do {
-          max -= 100;
+	for (let i of cid) {
+    		if (i[0] == 'ONE HUNDRED') {
+      		let count = 0;
+	      	let max = i[1];
+     		 do {
+          	max -= 100;
         //   finalChange -= moneyValues['ONE HUNDRED'];
-          count += 1;
+          	count += 1;
         //   finalChange -= 100;
       }
       while (max > 0);
@@ -134,8 +127,27 @@ function checkCashRegister(price, cash, cid) {
         console.log(`pennies in cash register: ${count}`);
     }
 }
-		// code
+
+	console.log('Math.floor(change/100))',Math.floor(change / 100));
+	console.log('Math.floor(change/20))',Math.floor(change / 20));
+	console.log('Math.floor(change/10))',Math.floor(change / 10));
+	console.log('Math.floor(change/5))',Math.floor(change / 5));
+	console.log('Math.floor(change/1))',Math.floor(change / 1));
+	console.log('Math.floor(change/.25))',Math.floor(change / .25));
+	console.log('Math.floor(change/.10))',Math.floor(change / .10));
+	console.log('Math.floor(change/.05))',Math.floor(change / .05));
+	console.log('Math.floor(change/.01))',Math.floor(change / .01));
+
+
+	if (sum < change || change % sum !== 0) {
+		output.status = 'INSUFFICIENT_FUNDS';
+		return output;
 	}
+	else if (change === sum) {
+		output.status = 'CLOSED';
+		output.change = cid;
+	} 
+		// code
 
 
 	
